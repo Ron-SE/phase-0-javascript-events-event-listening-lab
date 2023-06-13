dbcecf25e22e4fda44f89f58fdb92193fa9b71bb
Ron <ronmwangi00@gmail.com>
2023-06-13 13:05:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button= document.getElementById('button');
+    function buttonClick() {
+    alert('I was clicked!');
+    }
+    button.addEventListener('click', buttonClick);
 }
+addingEventListener();
